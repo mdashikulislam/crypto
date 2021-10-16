6f4d3dada61b8b62dba1a22b4b26cab85a8570cd
mdashikulislam <ashik.nwu@gmail.com>
2021-10-17 03:42:48 +0600

    api test

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -26,7 +26,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
             $schedule->command('find:winner')
-                ->dailyAt('23:38')
+                ->dailyAt('22:00')
                 ->timezone(config('app.timezone'));
     }
 
